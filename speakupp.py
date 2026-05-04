180bb3e182cd00c0b45ffda81686e6026ab5b47f
sanaallaudeenshaikh2001 <sanashaikhd1245@gmail.com>
2026-05-05 00:47:31 +0530

    Add NEW FILES

---
+++ speakupp.py
@@ -0,0 +1,16 @@
+import pyttsx3
+
+if __name__ == "__main__":
+    print("Welcome to RoboSpeaker created by Sana")
+
+    engine = pyttsx3.init()
+
+    while True:
+        text = input("Enter what you want me to speak (type 'exit' to stop): ")
+
+        if text.lower() == "exit":
+            print("Goodbye 👋")
+            break
+
+        engine.say(text)
+        engine.runAndWait()
